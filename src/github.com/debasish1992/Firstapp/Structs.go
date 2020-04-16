8ea15a2906d9b05e78bffcde50dbceedce65081b
Debasish Das <plastic.dendrite@yahoo.com>
2020-04-17 00:36:37 +0530

    Struct and Pointer Implementations

--- src/github.com/debasish1992/Firstapp/Structs.go
+++ src/github.com/debasish1992/Firstapp/Structs.go
@@ -1,39 +1,75 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Doctor struct {
-	number    int
-	actorName string
-	companion []string
-	episodes  []string
-}
+// // type Doctor struct {
+// // 	number    int
+// // 	actorName string
+// // 	companion []string
+// // 	episodes  []string
+// // }
 
-func main() {
-	aDoctor := Doctor{
-		number:    1002,
-		actorName: "John Patwee",
-		//episodes:  []string{},
-		companion: []string{
-			"Hayden",
-			"Lisa",
-			"Simons",
-			"Denver",
-		},
-	}
+// func main() {
+// 	// aDoctor := Doctor{
+// 	// 	number:    1002,
+// 	// 	actorName: "John Patwee",
+// 	// 	//episodes:  []string{},
+// 	// 	companion: []string{
+// 	// 		"Hayden",
+// 	// 		"Lisa",
+// 	// 		"Simons",
+// 	// 		"Denver",
+// 	// 	},
+// 	// }
 
-	fmt.Println(aDoctor)
-	fmt.Println(aDoctor.actorName)
-	fmt.Println(aDoctor.number)
-	fmt.Println(aDoctor.companion)
+// 	// fmt.Println(aDoctor)
+// 	// fmt.Println(aDoctor.actorName)
+// 	// fmt.Println(aDoctor.number)
+// 	// fmt.Println(aDoctor.companion)
 
-	anonymousStruct()
-}
+// 	// anonymousStruct()
+// 	//solveLooping()
+// 	//deferImple()
 
-func anonymousStruct() {
-	bDoctor := struct{ name string }{name: "Denver Kumar"}
-	anotherDoctor := bDoctor
-	anotherDoctor.name = "John bakker"
-	fmt.Print(bDoctor)
-	fmt.Print(anotherDoctor)
-}
+// 	panicImpl()
+// }
+
+// func anonymousStruct() {
+// 	// bDoctor := struct{ name string }{name: "Denver Kumar"}
+// 	// anotherDoctor := bDoctor
+// 	// anotherDoctor.name = "John bakker"
+// 	// fmt.Print(bDoctor)
+// 	// fmt.Print(anotherDoctor)
+// }
+
+// func solveLooping() {
+// 	i := 1
+// 	for i < 10 {
+// 		if i == 9 {
+// 			break
+// 		}
+
+// 		for j := 0; j < 10; j++ {
+// 			fmt.Println(i * j)
+// 		}
+// 	}
+// }
+
+// func deferImple() {
+
+// 	// defer keyword statement execute at kast of the function
+// 	defer fmt.Println("Start")
+// 	defer fmt.Println("Middle")
+// 	defer fmt.Println("End")
+
+// 	a := "start"
+// 	// This line will print start not end
+// 	defer fmt.Println(a)
+// 	a = "end"
+// }
+
+// func panicImpl() {
+// 	defer fmt.Println("Start")
+// 	defer fmt.Println("Middle")
+// 	defer fmt.Println("End")
+// }
